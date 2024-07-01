a3f2c029903bacc2c30496ea5b5b26a0cfdc3c2e
Harshavardhana P <harshavardhana.p@winwire.com>
2024-07-01 12:10:03 +0530

    types: adding types to the functions

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { factory } from "./factory";
 
 // DOM elements
-const startAtControl = document.getElementById("start-at") as HTMLInputElement;
-const stepControl = document.getElementById("step") as HTMLInputElement;
-const countButton = document.querySelector(
+const startAtControl: HTMLInputElement = document.getElementById(
+  "start-at"
+) as HTMLInputElement;
+const stepControl: HTMLInputElement = document.getElementById(
+  "step"
+) as HTMLInputElement;
+const countButton: HTMLButtonElement = document.querySelector(
   ".count_button"
 ) as HTMLButtonElement;
-const currentCount = document.querySelector(
+const currentCount: HTMLSpanElement = document.querySelector(
   ".current_count"
 ) as HTMLSpanElement;
 
 // Function to update count based on factory function and reset counter
-function updateCountAndResetCounter() {
+function updateCountAndResetCounter(): void {
   const startValue = Number(startAtControl.value);
   const stepValue = 0; // resetting the step value to '0' to ensure that, before clicking the counter button it should not add the step value during the reset.
   const result = factory(startValue, stepValue);
